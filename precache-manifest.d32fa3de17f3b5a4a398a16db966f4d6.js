7ab1db7159bdc41773b5392c40967e6905e8e8ea
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-10 09:04:07 +0200

    Updates

---
+++ precache-manifest.d32fa3de17f3b5a4a398a16db966f4d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58b555dce84d7c5a97beea7b15091e8",
+    "url": "/Bobian/index.html"
+  },
+  {
+    "revision": "6c52aa14fa3a0a2f3f95",
+    "url": "/Bobian/static/css/main.b3cc3f63.chunk.css"
+  },
+  {
+    "revision": "0aa65e4963a0a820607e",
+    "url": "/Bobian/static/js/2.84b2fc27.chunk.js"
+  },
+  {
+    "revision": "6c52aa14fa3a0a2f3f95",
+    "url": "/Bobian/static/js/main.88ee5abc.chunk.js"
+  },
+  {
+    "revision": "3177ed4925f36323f17b",
+    "url": "/Bobian/static/js/runtime-main.caf197a4.js"
+  }
+]);
